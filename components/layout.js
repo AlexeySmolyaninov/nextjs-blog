970d6588fe491d955d9568339fbed9b6b4b007bf
Alexey Smolyaninov <alexey.smolyaninov@fondion.com>
2023-08-16 14:27:19 +0200

    nextjs-blog: added full nickname

--- components/layout.js
+++ components/layout.js
@@ -4,7 +4,7 @@ import styles from "./layout.module.css";
 import utilStyles from "../styles/utils.module.css";
 import Link from "next/link";
 
-const name = "Alex";
+const name = "Alex Smol";
 export const siteTitle = "Next.js Sample Website";
 
 export default function Layout({ children, home }) {
