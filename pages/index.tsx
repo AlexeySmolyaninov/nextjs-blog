356f60f01db4446833034e9b0fc4cfe8ddbec71f
Alexey Smolyaninov <alexey.smolyaninov@fondion.com>
2023-08-16 15:26:17 +0200

    nextjs-blog: converted files to use typescript

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,7 @@
 import Head from "next/head";
 import Layout, { siteTitle } from "../components/layout";
 import utilStyles from "../styles/utils.module.css";
-import { getSortedPostsData } from "../lib/posts";
+import { Post, getSortedPostsData } from "../lib/posts";
 import Link from "next/link";
 import Date from "../components/date";
 
@@ -14,7 +14,11 @@ export async function getStaticProps() {
   };
 }
 
-export default function Home({ allPostsData }) {
+interface HomeProps {
+  allPostsData: ReturnType<typeof getSortedPostsData>;
+}
+
+export default function Home({ allPostsData }: HomeProps) {
   return (
     <Layout home>
       <Head>
